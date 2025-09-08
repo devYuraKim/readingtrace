5c06b908044e6902df6f432710e1049e8675a6ae
devYuraKim <dev.yurakim@gmail.com>
2025-09-08 22:00:25 +0900

    chore: remove unused @EnableAsync

--- backend/src/main/java/com/yurakim/readingtrace/ReadingtraceApplication.java
+++ backend/src/main/java/com/yurakim/readingtrace/ReadingtraceApplication.java
@@ -3,9 +3,7 @@ package com.yurakim.readingtrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableAsync
 @EnableJpaAuditing(auditorAwareRef = "AuditorAwareImpl")
 @SpringBootApplication
 public class ReadingtraceApplication {
