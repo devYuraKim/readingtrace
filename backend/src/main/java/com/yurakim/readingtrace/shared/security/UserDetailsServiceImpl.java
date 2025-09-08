76acb18f471c839d22339fb9465a4a57604932d5
devYuraKim <dev.yurakim@gmail.com>
2025-09-08 10:20:09 +0900

    fix(auth): add ROLE_ prefix when mapping roles to GrantedAuthority

--- backend/src/main/java/com/yurakim/readingtrace/shared/security/UserDetailsServiceImpl.java
+++ backend/src/main/java/com/yurakim/readingtrace/shared/security/UserDetailsServiceImpl.java
@@ -24,7 +24,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         User user = userRepository.findByEmail(email).orElseThrow(
                 ()-> new UsernameNotFoundException("User not found for " + email)
         );
-        Set<GrantedAuthority> authorities = user.getRoles().stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toSet());
+        Set<GrantedAuthority> authorities = user.getRoles().stream().map(role -> new SimpleGrantedAuthority("ROLE_"+role.getName())).collect(Collectors.toSet());
         return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
     }
 }
