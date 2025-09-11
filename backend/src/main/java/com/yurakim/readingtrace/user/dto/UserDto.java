0293f413a20fce14a39b6a63514004cf2d1a2fd7
devYuraKim <dev.yurakim@gmail.com>
2025-09-11 10:07:17 +0900

    feat(admin): add GET /get-all-users and POST /update-user-roles/{id}

---
+++ backend/src/main/java/com/yurakim/readingtrace/user/dto/UserDto.java
@@ -0,0 +1,27 @@
+package com.yurakim.readingtrace.user.dto;
+
+import com.yurakim.readingtrace.user.entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class UserDto {
+
+    private String email;
+    private Set<Role> roles;
+    private LocalDateTime lastLoginAt;
+    private LocalDateTime lastFailedLoginAt;
+    private Integer failedLoginAttempts = 0;
+    private String failedLoginReason;
+    private Boolean isAccountLocked = false;
+    private LocalDateTime accountLockedAt;
+    private LocalDateTime accountUnlockedAt;
+    private String signUpMethod;
+
+}
--- backend/src/main/java/com/yurakim/readingtrace/user/service/serviceImpl/UserServiceImpl.java
+++ backend/src/main/java/com/yurakim/readingtrace/user/service/serviceImpl/UserServiceImpl.java
@@ -1,22 +1,79 @@
 package com.yurakim.readingtrace.user.service.serviceImpl;
 
 import com.yurakim.readingtrace.auth.dto.LoginResponseDto;
+import com.yurakim.readingtrace.user.dto.UserDto;
+import com.yurakim.readingtrace.user.entity.Role;
 import com.yurakim.readingtrace.user.entity.User;
+import com.yurakim.readingtrace.user.repository.RoleRepository;
 import com.yurakim.readingtrace.user.repository.UserRepository;
 import com.yurakim.readingtrace.user.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @AllArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
     @Override
     public LoginResponseDto getUser(Long id) {
         User user = userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException("User not found"));
         return new LoginResponseDto(user.getEmail());
+    }
+
+    //TODO: Set a mapper
+    @Override
+    public List<UserDto> getAllUsers() {
+        List<User> allUsersEntity = userRepository.findAll();
+        List<UserDto> allUsersDto = allUsersEntity.stream().map(
+                user -> {
+                    UserDto userDto = new UserDto();
+                    userDto.setEmail(user.getEmail());
+                    userDto.setRoles(user.getRoles());
+                    userDto.setLastLoginAt(user.getLastLoginAt());
+                    userDto.setLastFailedLoginAt(user.getLastFailedLoginAt());
+                    userDto.setFailedLoginAttempts(user.getFailedLoginAttempts());
+                    userDto.setFailedLoginReason(user.getFailedLoginReason());
+                    userDto.setIsAccountLocked(user.getIsAccountLocked());
+                    userDto.setAccountLockedAt(user.getAccountLockedAt());
+                    userDto.setAccountUnlockedAt(user.getAccountUnlockedAt());
+                    userDto.setSignUpMethod(user.getSignUpMethod());
+                    return userDto;
+                }
+        ).toList();
+        return allUsersDto;
+    }
+
+    @Override
+    public UserDto updateUserRoles(Long id, List<String> roleList) {
+        User user = userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        Set<Role> roles = roleList.stream()
+                .map(roleName -> roleRepository.findByName(roleName)
+                        .orElseThrow(() -> new IllegalArgumentException("Invalid role: " + roleName)))
+                .collect(Collectors.toSet());
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        UserDto userDto = new UserDto();
+        userDto.setEmail(user.getEmail());
+        userDto.setRoles(user.getRoles());
+        userDto.setLastLoginAt(user.getLastLoginAt());
+        userDto.setLastFailedLoginAt(user.getLastFailedLoginAt());
+        userDto.setFailedLoginAttempts(user.getFailedLoginAttempts());
+        userDto.setFailedLoginReason(user.getFailedLoginReason());
+        userDto.setIsAccountLocked(user.getIsAccountLocked());
+        userDto.setAccountLockedAt(user.getAccountLockedAt());
+        userDto.setAccountUnlockedAt(user.getAccountUnlockedAt());
+        userDto.setSignUpMethod(user.getSignUpMethod());
+
+        return userDto;
     }
 }
