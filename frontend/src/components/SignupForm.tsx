60ec1e1547b4591e53e1b26a78f865e713ac750d
devYuraKim <dev.yurakim@gmail.com>
2025-09-14 18:19:18 +0900

    setup: add base components and navigation links for login and signup

---
+++ frontend/src/components/SignupForm.tsx
@@ -0,0 +1,71 @@
+import { NavLink } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { cn } from '@/lib/utils';
+
+export function SignupForm({
+  className,
+  ...props
+}: React.ComponentProps<'div'>) {
+  return (
+    <div className={cn('flex flex-col gap-6', className)} {...props}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Create your account</CardTitle>
+          <CardDescription>Join our community of readers</CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <form>
+            <div className="grid gap-6">
+              <div className="grid gap-6">
+                <div className="grid gap-3">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="example@mail.com"
+                    required
+                  />
+                </div>
+                <div className="grid gap-3">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                    <a
+                      href="#"
+                      className="ml-auto text-sm underline-offset-4 hover:underline"
+                    >
+                      Forgot your password?
+                    </a>
+                  </div>
+                  <Input id="password" type="password" required />
+                </div>
+                <Button type="submit" className="w-full">
+                  Sign up
+                </Button>
+              </div>
+              <div className="text-center text-sm">
+                Already have an account?{' '}
+                <NavLink to="/login" className="underline underline-offset-4">
+                  Log in
+                </NavLink>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{' '}
+        and <a href="#">Privacy Policy</a>.
+      </div>
+    </div>
+  );
+}
--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,20 +1,22 @@
-import PrivateLayout from "@/layouts/PrivateLayout";
-import PublicLayout from "@/layouts/PublicLayout";
-import Landing from "@/pages/Landing";
-import Login from "@/pages/Login";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
 } from "react-router-dom";
+import PrivateLayout from "@/layouts/PrivateLayout";
+import PublicLayout from "@/layouts/PublicLayout";
+import Landing from "@/pages/Landing";
+import LoginPage from "@/pages/LoginPage";
+import SignupPage from "@/pages/SignupPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       {/* Public routes */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
       <Route path="/" element={<PublicLayout />}>
         <Route index element={<Landing />} />
-        <Route path="/login" element={<Login />} />
       </Route>
 
       {/* Protected routes */}
