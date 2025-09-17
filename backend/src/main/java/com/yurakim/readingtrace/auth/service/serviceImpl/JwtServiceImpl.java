43a0781678b3016750a23404251d6737245f1875
devYuraKim <dev.yurakim@gmail.com>
2025-09-17 11:27:51 +0900

    feat(auth): introduce hashing for refresh token, deprecate redundant isValidRefreshToken method

--- backend/src/main/java/com/yurakim/readingtrace/auth/service/serviceImpl/JwtServiceImpl.java
+++ backend/src/main/java/com/yurakim/readingtrace/auth/service/serviceImpl/JwtServiceImpl.java
@@ -2,9 +2,11 @@ package com.yurakim.readingtrace.auth.service.serviceImpl;
 
 import com.yurakim.readingtrace.auth.constant.JWT;
 import com.yurakim.readingtrace.auth.entity.entity.RefreshToken;
+import com.yurakim.readingtrace.auth.enums.InvalidationCause;
 import com.yurakim.readingtrace.auth.exception.RefreshTokenException;
 import com.yurakim.readingtrace.auth.repository.RefreshTokenRepository;
 import com.yurakim.readingtrace.auth.service.JwtService;
+import com.yurakim.readingtrace.auth.util.Sha256Hashing;
 import com.yurakim.readingtrace.user.entity.User;
 import com.yurakim.readingtrace.user.repository.UserRepository;
 import io.jsonwebtoken.Claims;
@@ -13,6 +15,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -30,6 +33,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class JwtServiceImpl implements JwtService {
@@ -73,7 +77,7 @@ public class JwtServiceImpl implements JwtService {
                 ? Date.from(rotateExpiry.atZone(ZoneId.systemDefault()).toInstant())
                 : new Date(now.getTime() + expiration);
 
-        String refreshToken = Jwts.builder()
+        String rawRefreshToken = Jwts.builder()
                 .issuer(issuer)
                 .subject(user.getEmail())
                 .issuedAt(now)
@@ -81,46 +85,36 @@ public class JwtServiceImpl implements JwtService {
                 .signWith(secretKey)
                 .id(UUID.randomUUID().toString())
                 .compact();
+        String hashedRefreshToken = Sha256Hashing.generateSHA256Hash(rawRefreshToken);
 
         LocalDateTime expiryLocalDateTime = expiry.toInstant()
                 .atZone(ZoneId.systemDefault())
                 .toLocalDateTime();
 
-        RefreshToken refreshTokenDB = new RefreshToken();
-        refreshTokenDB.setTokenHash(refreshToken);
-        refreshTokenDB.setUserId(user.getId());
-        refreshTokenDB.setExpiresAt(expiryLocalDateTime);
-        refreshTokenRepository.save(refreshTokenDB);
+        RefreshToken newRefreshToken = new RefreshToken();
+        newRefreshToken.setTokenHash(hashedRefreshToken);
+        newRefreshToken.setUserId(user.getId());
+        newRefreshToken.setExpiresAt(expiryLocalDateTime);
+        refreshTokenRepository.save(newRefreshToken);
 
-        return refreshToken;
+        return rawRefreshToken;
     }
 
     @Override
-    public Boolean isValidRefreshToken(String refreshToken) {
-        RefreshToken existing = refreshTokenRepository.findByTokenHash(refreshToken).orElseThrow(
-                () -> new RefreshTokenException(JWT.INVALID_REFRESH_TOKEN, "Refresh token not found")
-        );
-        if(!existing.isRevoked() && !existing.getExpiresAt().isBefore(LocalDateTime.now())) {
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public String getSubject(String refreshToken) {
+    public String getSubject(String rawRefreshToken) {
         String secret = environment.getProperty(JWT.REFRESH_SECRET_KEY_PROPERTY);
         SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
-        Claims claims = Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(refreshToken).getPayload();
+        Claims claims = Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(rawRefreshToken).getPayload();
         return claims.getSubject();
     }
     //assert Jwts.parser().verifyWith(key).build().parseSignedClaims(jws).getPayload().getSubject().equals("Joe");
 
     @Override
-    public String refreshAccessToken(String refreshToken) {
-        if (!isValidRefreshToken(refreshToken)) {
+    public String refreshAccessToken(String rawRefreshToken) {
+        if (validateRefreshToken(rawRefreshToken) == null) {
             throw new RuntimeException("Invalid refresh token");
         }
-        String userEmail = this.getSubject(refreshToken);
+        String userEmail = this.getSubject(rawRefreshToken);
         User user = userRepository.findByEmail(userEmail).orElseThrow(() -> new RuntimeException("User not found"));
 
         Authentication authentication = new UsernamePasswordAuthenticationToken(userEmail, null, user.getRoles().stream().map(role -> new SimpleGrantedAuthority("ROLE_"+role.getName())).collect(Collectors.toList()));
@@ -129,32 +123,48 @@ public class JwtServiceImpl implements JwtService {
     }
 
     @Override
-    public RefreshToken validateRefreshToken(String refreshToken) {
+    public RefreshToken validateRefreshToken(String rawRefreshToken) {
+        String hashedRefreshToken = Sha256Hashing.generateSHA256Hash(rawRefreshToken);
         //1.Check DB
-        RefreshToken existing = refreshTokenRepository.findByTokenHash(refreshToken).orElseThrow(
+        RefreshToken existingRecord = refreshTokenRepository.findByTokenHash(hashedRefreshToken).orElseThrow(
                 () -> new RefreshTokenException(JWT.INVALID_REFRESH_TOKEN, "Refresh token not found in DB")
         );
 
-        //2.Check revocation/expiry in DB
-        if(existing.isRevoked()) {
-            throw new RefreshTokenException(JWT.REVOKED_REFRESH_TOKEN, "Refresh token has been revoked");
+        //2.Check revocation
+        if(existingRecord.isRevoked()) {
+            throw new RefreshTokenException(JWT.REVOKED_REFRESH_TOKEN, "Refresh token has been revoked due to: "+existingRecord.getInvalidationCause());
         }
-        if( existing.getExpiresAt().isBefore(LocalDateTime.now())){
+        //3.Check expiry
+        if(existingRecord.getExpiresAt().isBefore(LocalDateTime.now())){
+            if(existingRecord.getInvalidationCause() == InvalidationCause.NONE) {
+                existingRecord.setInvalidationCause(InvalidationCause.EXPIRED);
+                refreshTokenRepository.save(existingRecord);
+            }
             throw new RefreshTokenException(JWT.EXPIRED_REFRESH_TOKEN, "Refresh token has expired");
         }
-
         //3.Verify JWT signature
         String secret = environment.getProperty(JWT.REFRESH_SECRET_KEY_PROPERTY);
         SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
         try {
-            Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(refreshToken);
+            Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(rawRefreshToken);
         } catch (JwtException e) {
-            throw new JwtException("Invalid refresh token: " + e.getMessage());
+            existingRecord.setRevoked(true);
+            existingRecord.setInvalidationCause(InvalidationCause.COMPROMISED);
+            refreshTokenRepository.save(existingRecord);
+            log.warn("Refresh token has been compromised: JWT signature invalid || userId: {}, refreshTokenId: {}, refreshToken: {}, timestamp: {}", existingRecord.getUserId(), existingRecord.getId(), existingRecord.getTokenHash(), LocalDateTime.now());
+            throw new RefreshTokenException(JWT.COMPROMISED_REFRESH_TOKEN, "Refresh token has been compromised");
+        }
+        //4.Verify hash
+        if(!existingRecord.getTokenHash().equals(hashedRefreshToken)){
+            existingRecord.setRevoked(true);
+            existingRecord.setInvalidationCause(InvalidationCause.COMPROMISED);
+            refreshTokenRepository.save(existingRecord);
+            log.warn("Refresh token has been compromised: Hash mismatch || userId: {}, refreshTokenId: {}, refreshToken: {}, timestamp: {}", existingRecord.getUserId(), existingRecord.getId(), existingRecord.getTokenHash(), LocalDateTime.now());
+            throw new RefreshTokenException(JWT.COMPROMISED_REFRESH_TOKEN, "Refresh token has been compromised");
         }
         //TODO: validate user
-
         //4.Return existing refresh token
-        return existing;
+        return existingRecord;
     }
 
     @Transactional
@@ -170,7 +180,8 @@ public class JwtServiceImpl implements JwtService {
         User user = userRepository.findByEmail(userEmail).orElseThrow(() -> new RuntimeException("User not found"));
         //4. generate new refresh token and revoke old one
         String newRefreshTokenStr = this.generateRefreshToken(user, originalExpiry);
-        RefreshToken newRefreshToken = refreshTokenRepository.findByTokenHash(newRefreshTokenStr).orElseThrow(() -> new RefreshTokenException(JWT.INVALID_REFRESH_TOKEN, "Refresh token not found in DB"));
+        String hashedRefreshToken = Sha256Hashing.generateSHA256Hash(newRefreshTokenStr);
+        RefreshToken newRefreshToken = refreshTokenRepository.findByTokenHash(hashedRefreshToken).orElseThrow(() -> new RefreshTokenException(JWT.INVALID_REFRESH_TOKEN, "Refresh token not found in DB"));
         existingToken.setRevoked(true);
         existingToken.setReplacedById(newRefreshToken.getId());
         refreshTokenRepository.save(existingToken);
@@ -205,6 +216,24 @@ public class JwtServiceImpl implements JwtService {
                     .compact();
         }
         throw new IllegalStateException("No authenticated user found");
+    }
+
+    @Deprecated
+    @Override
+    public Boolean isValidRefreshToken(String rawRefreshToken) {
+        String hashedRefreshToken = Sha256Hashing.generateSHA256Hash(rawRefreshToken);
+        RefreshToken existingRecord = refreshTokenRepository.findByTokenHash(hashedRefreshToken).orElseThrow(
+                () -> new RefreshTokenException(JWT.INVALID_REFRESH_TOKEN, "Refresh token not found")
+        );
+        if(existingRecord.isRevoked()){
+            return false;
+        }
+        if(existingRecord.getExpiresAt().isBefore(LocalDateTime.now())) {
+            existingRecord.setInvalidationCause(InvalidationCause.EXPIRED);
+            refreshTokenRepository.save(existingRecord);
+            return false;
+        }
+        return true;
     }
 
 }
