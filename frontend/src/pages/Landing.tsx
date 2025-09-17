70ab09f0d9cad2a620d05c01a0e76d92c92414cb
devYuraKim <dev.yurakim@gmail.com>
2025-09-17 17:16:33 +0900

    WIP(route): implement basic flow of redirecting user based on global state

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,6 +1,11 @@
+import { useAuthStore } from '@/store/useAuthStore';
+import { Navigate } from 'react-router-dom';
+
 const Landing = () => {
-    return (
-        <div>Landing</div>
-    )
-}
-export default Landing
+  // TODO: create isAuthenticated state
+  if (!useAuthStore((state) => state.user)) {
+    return <Navigate to="/login" />;
+  }
+  return <div>Landing</div>;
+};
+export default Landing;
--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,27 +1,8 @@
 import React from 'react';
-import { apiClient } from '@/queries/axios';
 import { useAuthStore } from '@/store/useAuthStore';
 
 const MainPage = () => {
-  const handleUserInfo = () => {
-    apiClient.get(`/user/${useAuthStore.getState().user?.id}`).then((res) => {
-      console.log(res);
-    });
-  };
-
-  const handleSomethingElse = () => {
-    apiClient.get('/user/11').then((res) => {
-      console.log(res);
-    });
-  };
-
-  return (
-    <div>
-      After login
-      <button onClick={handleUserInfo}>USER INFO</button>
-      <button onClick={handleSomethingElse}>SOMETHING ELSE</button>
-    </div>
-  );
+  return <div>hello, {useAuthStore.getState().user?.email}</div>;
 };
 
 export default MainPage;
