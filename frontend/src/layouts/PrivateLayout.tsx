b029978a34f23573c559dc29e40c99ec14ca5704
devYuraKim <dev.yurakim@gmail.com>
2025-09-17 16:56:28 +0900

    WIP(ui): assemble main page using ShadCN components

--- frontend/src/layouts/PrivateLayout.tsx
+++ frontend/src/layouts/PrivateLayout.tsx
@@ -1,12 +1,52 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
+import { AppSidebar } from '@/components/AppSidebar';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import { Separator } from '@/components/ui/separator';
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar';
 
-const PrivateLayout = () => {
+export default function PrivateLayout() {
   return (
-    <>
-      <div>PrivateLayout</div>;<div>header component here</div>
-      <div>sidebar component here</div>
-      <Outlet />
-    </>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="bg-background sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="#">
+                  Building Your Application
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4">
+          <Outlet />
+          {Array.from({ length: 24 }).map((_, index) => (
+            <div
+              key={index}
+              className="bg-muted/50 aspect-video h-12 w-full rounded-lg"
+            />
+          ))}
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
-};
-export default PrivateLayout;
+}
