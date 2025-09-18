6f0e860cb6ff62ecb86971d074a411d2bd054432
devYuraKim <dev.yurakim@gmail.com>
2025-09-18 21:14:03 +0900

    feat(book): introduce JpaSpecification for dynamic query building

--- backend/src/main/java/com/yurakim/readingtrace/book/repository/UserBookRepository.java
+++ backend/src/main/java/com/yurakim/readingtrace/book/repository/UserBookRepository.java
@@ -2,7 +2,8 @@ package com.yurakim.readingtrace.book.repository;
 
 import com.yurakim.readingtrace.book.entity.UserBook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface UserBookRepository extends JpaRepository<UserBook, Long> {
+public interface UserBookRepository extends JpaRepository<UserBook, Long>, JpaSpecificationExecutor<UserBook> {
 
 }
--- backend/src/main/java/com/yurakim/readingtrace/book/service/BookService.java
+++ backend/src/main/java/com/yurakim/readingtrace/book/service/BookService.java
@@ -2,7 +2,11 @@ package com.yurakim.readingtrace.book.service;
 
 import com.yurakim.readingtrace.book.dto.UserBookDto;
 
+import java.util.List;
+
 public interface BookService {
 
     void addUserBook(UserBookDto userBookDto);
+
+    List<UserBookDto> getUserBooks(Long userId, String status, String visibility, Integer rating);
 }
