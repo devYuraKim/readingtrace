56dd9e4075b3087d35e73a088558dc37573fe350
devYuraKim <dev.yurakim@gmail.com>
2025-09-19 23:20:55 +0900

    fix(test): remove hash value

---
+++ backend/src/test/java/com/yurakim/readingtrace/Sha256HashingTest.java
@@ -0,0 +1,23 @@
+package com.yurakim.readingtrace;
+
+import com.yurakim.readingtrace.auth.util.Sha256Hashing;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class Sha256HashingTest {
+
+    @Test
+    void testGenerateSHA256Hash() {
+        String originalRefreshToken = "";
+        String hash = Sha256Hashing.generateSHA256Hash(originalRefreshToken);
+
+        System.out.println("Generated hash: " + hash);
+
+        String expected = "";
+
+        assertEquals(expected, hash);
+    }
+}
