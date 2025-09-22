b442e6cd40a8207c34633abbe76d5762fa923922
devYuraKim <dev.yurakim@gmail.com>
2025-09-22 19:36:57 +0900

    feat(UserBookDto): add start/end date fields

--- backend/src/main/java/com/yurakim/readingtrace/book/dto/UserBookDto.java
+++ backend/src/main/java/com/yurakim/readingtrace/book/dto/UserBookDto.java
@@ -3,6 +3,8 @@ package com.yurakim.readingtrace.book.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 @Setter
 public class UserBookDto {
@@ -11,4 +13,6 @@ public class UserBookDto {
     private String status;
     private String visibility;
     private Integer rating;
+    private Date startDate;
+    private Date endDate;
 }
