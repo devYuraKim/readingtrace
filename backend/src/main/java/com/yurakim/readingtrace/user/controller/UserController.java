74998595ed0d7e73303f780606d9f960181cf9c9
devYuraKim <dev.yurakim@gmail.com>
2025-10-04 22:33:32 +0900

    refactor: change endpoint URLs to follow REST API endpoint design rules

--- backend/src/main/java/com/yurakim/readingtrace/user/controller/UserController.java
+++ backend/src/main/java/com/yurakim/readingtrace/user/controller/UserController.java
@@ -1,15 +1,14 @@
 package com.yurakim.readingtrace.user.controller;
 
 import com.yurakim.readingtrace.auth.dto.LoginResponseDto;
+import com.yurakim.readingtrace.book.dto.UserBookDto;
+import com.yurakim.readingtrace.book.service.BookService;
 import com.yurakim.readingtrace.shared.constant.ApiPath;
 import com.yurakim.readingtrace.user.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
@@ -17,6 +16,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+    private final BookService bookService;
+
+    @PostMapping("/{userId}/books/{bookId}")
+    public ResponseEntity<UserBookDto> addUserBook(@PathVariable Long userId, @PathVariable String bookId, @RequestBody UserBookDto userBookDto){
+        //let the path variables be the single source of truth
+        userBookDto.setUserId(userId);
+        userBookDto.setBookId(bookId);
+        bookService.addUserBook(userBookDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{userId}/books/{bookId}")
+    public ResponseEntity<UserBookDto> getUserBook(@PathVariable Long userId, @PathVariable String bookId){
+        UserBookDto userBookDto = bookService.getUserBook(userId, bookId);
+        return ResponseEntity.ok(userBookDto);
+    }
+
 
     @GetMapping("/{id}")
     public ResponseEntity<LoginResponseDto> getUser(@PathVariable("id") Long id, @AuthenticationPrincipal String email){
