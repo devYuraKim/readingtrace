dd0103317936a3639ab1e80f5a7d4b886cbcd734
devYuraKim <dev.yurakim@gmail.com>
2025-10-17 22:51:28 +0900

    feat(Book): add Book entity and DTO

--- backend/src/main/java/com/yurakim/readingtrace/book/entity/Book.java
+++ backend/src/main/java/com/yurakim/readingtrace/book/entity/Book.java
@@ -1,31 +1,47 @@
-package com.yurakim.readingtrace.book.dto;
+package com.yurakim.readingtrace.book.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class BookDto {
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@ToString(callSuper = true)
+@Entity
+public class Book {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    private Long bookId;
+    private String externalId; //GoogleBooksId
+
+    @Column(nullable = false)
     private String title;
+
     private String authors;
+
     private String imageURL;
+
     private String publisher;
+
     private String publishedDate;
+
     private String description;
+
     private String isbn10;
+
     private String isbn13;
+
     private Integer pageCount;
+
     private List<String> categories;
+
     private Double averageRating;
+
     private Long ratingsCount;
 
     //TODO: Dimensions, AccessInfo >> include these?
+
 }
