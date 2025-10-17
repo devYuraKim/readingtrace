3d0da54a1ef2c639268ab59914f25deef288b899
devYuraKim <dev.yurakim@gmail.com>
2025-10-17 14:34:28 +0900

    refactor(GoogleBooks): expand DTO to include more API fields

--- backend/src/main/java/com/yurakim/readingtrace/book/dto/GoogleBooksResponseDto.java
+++ backend/src/main/java/com/yurakim/readingtrace/book/dto/GoogleBooksResponseDto.java
@@ -31,6 +31,7 @@ public class GoogleBooksResponseDto {
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class VolumeInfo {
         private String title;
+        private String subtitle;
         private List<String> authors;
         private ImageLinks imageLinks;
         private String publisher;
@@ -38,13 +39,27 @@ public class GoogleBooksResponseDto {
         private String description;
         @JsonProperty("industryIdentifiers")
         private List<IndustryIdentifier> industryIdentifiers;
+        private Integer pageCount;
+        private Dimensions dimensions;
+        private String printType;
+        private String mainCategory;
+        private List<String> categories;
+        private Double averageRating;
+        private Long ratingsCount;
+        private String language;
+        private String infoLink;
+        private AccessInfo accessInfo;
     }
 
     @Getter
     @Setter
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class ImageLinks {
+        private String smallThumbnail;
         private String thumbnail;
+        private String small;
+        private String medium;
+        private String large;
     }
 
     @Getter
@@ -54,5 +69,40 @@ public class GoogleBooksResponseDto {
         private String type;
         private String identifier;
     }
+
+    @Getter @Setter @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Dimensions {
+        private String height;
+        private String width;
+        private String thickness;
+    }
+
+    @Getter @Setter @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class AccessInfo {
+        private String country;
+        private String viewability;
+        private Boolean embeddable;
+        private Boolean publicDomain;
+        private String textToSpeechPermission;
+        private Epub epub;
+        private Pdf pdf;
+        private String webReaderLink;
+        private String accessViewStatus;
+    }
+
+    @Getter @Setter @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Epub {
+        private Boolean isAvailable;
+        private String downloadLink;
+        private String acsTokenLink;
+    }
+
+    @Getter @Setter @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Pdf {
+        private Boolean isAvailable;
+        private String downloadLink;
+        private String acsTokenLink;
+    }
+
 
 }
