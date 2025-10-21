19823f4fc900dd62262d3f00aa55d14e6ed3c079
devYuraKim <dev.yurakim@gmail.com>
2025-10-21 17:02:48 +0900

    refactor(UserReadingStatus): ensure consistent field structure

--- backend/src/main/java/com/yurakim/readingtrace/book/dto/UserReadingStatusDto.java
+++ backend/src/main/java/com/yurakim/readingtrace/book/dto/UserReadingStatusDto.java
@@ -3,11 +3,13 @@ package com.yurakim.readingtrace.book.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Getter
 @Setter
 public class UserReadingStatusDto {
+    private Long userReadingStatusId;
     private Long userId;
     private Long shelfId;
     private Long bookId;
@@ -16,4 +18,7 @@ public class UserReadingStatusDto {
     private Integer rating;
     private Date startDate;
     private Date endDate;
+    private String userReadingStatusMetadata;
+    private LocalDateTime userReadingStatusCreatedAt;
+    private LocalDateTime userReadingStatusUpdatedAt;
 }
--- backend/src/main/java/com/yurakim/readingtrace/book/mapper/UserReadingStatusMapper.java
+++ backend/src/main/java/com/yurakim/readingtrace/book/mapper/UserReadingStatusMapper.java
@@ -9,39 +9,49 @@ public class UserReadingStatusMapper {
 
     public UserReadingStatus mapToEntity(UserReadingStatusDto ursDto) {
         UserReadingStatus urs = new UserReadingStatus();
+        urs.setId(ursDto.getUserReadingStatusId());
         urs.setUserId(ursDto.getUserId());
+        urs.setShelfId(ursDto.getShelfId());
         urs.setBookId(ursDto.getBookId());
         urs.setStatus(ursDto.getStatus());
         urs.setVisibility(ursDto.getVisibility());
         urs.setRating(ursDto.getRating());
-        urs.setShelfId(ursDto.getShelfId());
         urs.setStartDate(ursDto.getStartDate());
         urs.setEndDate(ursDto.getEndDate());
+        //TODO: check if setting metadata is needed
+        //urs.setMetadata(ursDto.getUserReadingStatusMetadata());
         return urs;
     }
 
     public UserReadingStatus mapToEntity(UserReadingStatusDto ursDto, UserReadingStatus urs) {
+        urs.setId(ursDto.getUserReadingStatusId());
         urs.setUserId(ursDto.getUserId());
+        urs.setShelfId(ursDto.getShelfId());
         urs.setBookId(ursDto.getBookId());
         urs.setStatus(ursDto.getStatus());
         urs.setVisibility(ursDto.getVisibility());
         urs.setRating(ursDto.getRating());
-        urs.setShelfId(ursDto.getShelfId());
         urs.setStartDate(ursDto.getStartDate());
         urs.setEndDate(ursDto.getEndDate());
+        //TODO: check if setting metadata is needed
+        //urs.setMetadata(ursDto.getUserReadingStatusMetadata());
         return urs;
     }
 
     public UserReadingStatusDto mapToDto(UserReadingStatus urs){
         UserReadingStatusDto ursDto = new UserReadingStatusDto();
+        ursDto.setUserReadingStatusId(urs.getId());
         ursDto.setUserId(urs.getUserId());
-        ursDto.setBookId(urs.getBookId());
         ursDto.setShelfId(urs.getShelfId());
+        ursDto.setBookId(urs.getBookId());
         ursDto.setStatus(urs.getStatus());
         ursDto.setVisibility(urs.getVisibility());
         ursDto.setRating(urs.getRating());
         ursDto.setStartDate(urs.getStartDate());
         ursDto.setEndDate(urs.getEndDate());
+        ursDto.setUserReadingStatusMetadata(urs.getMetadata());
+        ursDto.setUserReadingStatusCreatedAt(urs.getCreatedAt());
+        ursDto.setUserReadingStatusUpdatedAt(urs.getUpdatedAt());
         return ursDto;
     }
 
