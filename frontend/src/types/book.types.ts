937513564bf1573df5f7fea828f0dac666dcb355
devYuraKim <dev.yurakim@gmail.com>
2025-10-21 19:50:01 +0900

    refactor(book): separate static data and type

---
+++ frontend/src/types/book.types.ts
@@ -0,0 +1,18 @@
+export type BookDto = {
+  bookId: number | null;
+  externalId: string | null;
+  title: string | null;
+  authors: string[] | null;
+  imageLinks: string | null;
+  publisher: string | null;
+  publishedDate: string | null;
+  description: string | null;
+  isbn10: string | null;
+  isbn13: string | null;
+  pageCount: number | null;
+  mainCategory: string | null;
+  categories: string[] | null;
+  averageRating: number | null;
+  ratingsCount: number | null;
+  language: string | null;
+};
