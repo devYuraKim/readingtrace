e0616e411d41748c223e7824caae0c73d2a13306
devYuraKim <dev.yurakim@gmail.com>
2025-11-10 14:29:53 +0900

    WIP(onboarding): set up basic structure and step navigation

---
+++ frontend/src/components/onboarding/ProgressBar.tsx
@@ -0,0 +1,35 @@
+type ProgressBarProps = {
+  totalSteps: number;
+  currentStep: number;
+  className: string;
+};
+
+const ProgressBar = ({
+  totalSteps,
+  currentStep,
+  className,
+}: ProgressBarProps) => {
+  const percentage = Math.min(
+    Math.max((currentStep / totalSteps) * 100, 0),
+    100,
+  );
+
+  return (
+    <div className={`flex items-center space-x-2 ${className}`}>
+      {/* Bar */}
+      <div className="flex-1 h-4 bg-gray-100 rounded-sm overflow-hidden">
+        <div
+          className="h-4 bg-black rounded-sm transition-all duration-300"
+          style={{ width: `${percentage}%` }}
+        />
+      </div>
+
+      {/* Step text */}
+      <div className="text-sm text-gray-700 whitespace-nowrap">
+        Step <span className="font-bold">{currentStep}</span> / {totalSteps}
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
---
+++ frontend/src/pages/OnboardingPage.tsx
@@ -0,0 +1,76 @@
+import { ReactElement, useState } from 'react';
+import ReadingTraceIcon from '@/assets/readingtrace.svg';
+import { CircleChevronLeft, CircleChevronRight } from 'lucide-react';
+import ProgressBar from '@/components/onboarding/ProgressBar';
+import Step1 from '@/components/onboarding/Step1';
+import Step2 from '@/components/onboarding/Step2';
+import Step3 from '@/components/onboarding/Step3';
+import { Card } from '@/components/ui/card';
+
+const OnboardingPage = () => {
+  const [currentStep, setCurrentStep] = useState<number>(1);
+
+  const totalSteps = 3;
+
+  const steps: Record<number, ReactElement> = {
+    1: <Step1 />,
+    2: <Step2 />,
+    3: <Step3 />,
+  };
+
+  return (
+    <div className="bg-muted flex flex-col min-h-screen items-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-lg flex-col gap-6">
+        <div className="flex items-center gap-1.5 self-center font-medium text-xl">
+          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-sm">
+            <img src={ReadingTraceIcon} alt="icon" className="size-4" />
+          </div>
+          ReadingTrace
+        </div>
+
+        <Card className="flex flex-col items-center justify-center">
+          {/* Navigation + Progress */}
+          <div className="flex items-center w-full max-w-md space-x-4">
+            {/* Previous Step */}
+            <CircleChevronLeft
+              className={`${
+                currentStep === 1
+                  ? 'stroke-gray-300'
+                  : 'cursor-pointer stroke-accent-foreground/70 hover:stroke-accent-foreground'
+              }`}
+              onClick={() =>
+                currentStep > 1 && setCurrentStep((prev) => prev - 1)
+              }
+            />
+
+            {/* Progress Bar */}
+            <ProgressBar
+              totalSteps={totalSteps}
+              currentStep={currentStep}
+              className="flex-1"
+            />
+
+            {/* Next Step */}
+            <CircleChevronRight
+              className={`${
+                currentStep === totalSteps
+                  ? 'stroke-gray-300'
+                  : 'cursor-pointer stroke-accent-foreground/70 hover:stroke-accent-foreground'
+              }`}
+              onClick={() =>
+                currentStep < totalSteps && setCurrentStep((prev) => prev + 1)
+              }
+            />
+          </div>
+
+          {/* Step content */}
+          <div className="w-full max-w-md border rounded-md p-6 shadow-sm text-center">
+            {steps[currentStep]}
+          </div>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default OnboardingPage;
