226f0f2f54dae17a609d7619e638ecd6fcbf77f7
devYuraKim <dev.yurakim@gmail.com>
2025-12-08 16:34:11 +0900

    fix(onboarding): always redirect to onboarding page if not completed

--- frontend/src/router/AuthenticatedRoutes.tsx
+++ frontend/src/router/AuthenticatedRoutes.tsx
@@ -1,10 +1,12 @@
 import { useAuthStore } from '@/store/useAuthStore';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { Spinner } from '@/components/ui/spinner';
 
 const AuthenticatedRoutes = () => {
   const user = useAuthStore((state) => state.user);
   const isAuthChecked = useAuthStore((state) => state.isAuthChecked);
+  const userProfile = useAuthStore((state) => state.userProfile);
+  const location = useLocation();
 
   if (!isAuthChecked) {
     return <Spinner />;
@@ -12,6 +14,14 @@ const AuthenticatedRoutes = () => {
 
   if (!user) {
     return <Navigate to="/login" />;
+  }
+
+  //TODO: find cleaner and more robust way to handle onboarding cases
+  if (
+    !userProfile?.isOnboardingCompleted &&
+    !location.pathname.includes('/onboarding')
+  ) {
+    return <Navigate to={`/users/${user.userId}/onboarding`} />;
   }
 
   return <Outlet />;
